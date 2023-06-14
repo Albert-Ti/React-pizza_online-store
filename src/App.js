e71f05b248afad146d37926b9309f539bff81d28
Albert Taygibov <albert.ti.2017@yandex.ru>
2023-06-15 00:09:05 +0300

    float: add api-request

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import './scss/app.scss'
+import React from 'react';
+import './scss/app.scss';
 import Header from './components/Header';
 import Categories from './components/Cotegories';
 import Sort from './components/Sort';
 import PizzaCard from './components/PizzaCard';
-import React from 'react';
-import dataPizzas from './pizzas.json';
 
 
 function App() {
+
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://648a12a25fa58521cab0be8c.mockapi.io/items')
+      .then(res => res.json())
+      .then(setItems)
+      .catch(err => alert('Произошла ошибка при загрузке данных с сервера!'))
+  }, []);
 
   return (
     <div className="wrapper">
@@ -15,13 +23,13 @@ function App() {
       <div className="content">
         <div className="container">
           <div className="content__top">
-            {/* <Categories /> */}
+            <Categories />
             <Sort />
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
             {
-              dataPizzas.map(item => (
+              items.map(item => (
                 <PizzaCard
                   key={item.id}
                   {...item}
